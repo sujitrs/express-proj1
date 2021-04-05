f5df299a378dfafe55d77c1b8d46850eaaba5560
sujeet s <suryawanshisujeet@gmail.com>
2021-04-05 06:27:35 +0530

    middleware added for logging

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const dotenv=require('dotenv');
 const routes=require('./routes');
 
+const app=express();
+
+const port=process.env.PORT||5000;
+const logger=(req,res,next)=>{
+    console.log(`${req.method} called ${req.url}`);
+    next();
+}
 
 dotenv.config({path:'./config/config.env'});
-const app=express();
-const port=process.env.PORT||5000;
+
 app.use('/api/v1/bootcamp',routes);
+app.use(logger);
+
+
 app.listen(port,console.log(`Server in ${process.env.NODE_ENV} on ${port}`));
